987e126739d5a817c71af19c44e6e6beb6e5043e
Samarth Patel <samarthxpatel@gmail.com>
2018-10-25 17:11:39 -0400

    V4
    -Images generated in one channel.
    -Now logs info to console
    -Generated 160x80 image quite nicely.  Saved images as 'initialized' and 'output'

--- ImageMake.py
+++ ImageMake.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy.misc as smp
 
-def makeImage(letterMat):
+def makeImage(letterMat, name):
     width = len(letterMat[0])
     height = len(letterMat)
 
@@ -9,7 +9,10 @@ def makeImage(letterMat):
 
     for y in range(len(letterMat)):
         for x in range(len(letterMat[y])):
-            data[y, x] = [((ord(letterMat[y][x]) - 80) * 200),((ord(letterMat[y][x]) - 80) * 200),((ord(letterMat[y][x]) - 80) * 200)]
+            pixel = [((ord(letterMat[y][x]) - 80) * 200),((ord(letterMat[y][x]) - 80) * 200),((ord(letterMat[y][x]) - 80) * 200)]
+            data[y, x] = pixel
 
     img = smp.toimage( data )       # Create a PIL image
+    img = img.resize((8*width, 8*height))
     img.show()                      # View in default viewer
+    img.save(name)
--- master.py
+++ master.py
@@ -1,28 +1,53 @@
 from ga import *
+import ImageMake
 
-grid = [Model(200, "a" * 200) for i in range(200)]
+def generateCurrentImage(name):
+    a = []
+    for row in grid:
+        elements = [str(item) for item in row.BEST_SPECIES]
+        a.append(elements)
+    ImageMake.makeImage(a, name)
+
+print("Initializing population")
+grid = [Model(200, "aaaaazzzzz" * 2) for i in range(10)]
+print("Population initialized")
+
 finished = [False]  * len(grid)
+gridRows = len(grid)
+
 
 for i in range(1):
-#    print('Generation {:2d} '.format(i))
+    generationError = 0
+    print('Generation {:<30}'.format(str(i)))
     for index, a in enumerate(grid):
         if finished[index] == False:
             a.nextGeneration(30, .1)
             error = a.BEST_ERROR
+            generationError += error
             if error == 0.0:
                 finished[index] = True
-            print('----------------------------------')
-            print(
-            '''Generation: {}
-            Error     : {:g}
-            Sequence  : {}'''.format(i, error, a.BEST_SPECIES))
+            percentComplete = int((index / gridRows) * 30) + 1
+            print('\r|{:<30}|'.format('*'*percentComplete), end='')
+    print('\r',end='')
     if not False in finished:
         break
 
-a = []
-for row in grid:
-    elements = [str(item) for item in row.BEST_SPECIES]
-    a.append(elements)
+generateCurrentImage('initalized.jpg')
 
-import ImageMake
-ImageMake.makeImage(a)
+for i in range(100):
+    generationError = 0
+    print('Generation {:<30}'.format(str(i)))
+    for index, a in enumerate(grid):
+        if finished[index] == False:
+            a.nextGeneration(30, .1)
+            error = a.BEST_ERROR
+            generationError += error
+            if error == 0.0:
+                finished[index] = True
+            percentComplete = int((index / gridRows) * 30) + 1
+            print('\r|{:<30}|'.format('*'*percentComplete), end='')
+    print('\r',end='')
+    if not False in finished:
+        break
+
+generateCurrentImage('output.jpg')
